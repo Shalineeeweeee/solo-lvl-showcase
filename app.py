fd693dc308171a42c261e376db6ee76f89efd683
Shalineeeweeee <shalineemishra2006@gmail.com>
2025-06-29 13:20:48 +0530

    Updated app.py and character_selector.html

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, j
 import json
 import os
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='/static', static_folder='static')
 app.secret_key = 'super-secret-key'
 
 # Load characters
